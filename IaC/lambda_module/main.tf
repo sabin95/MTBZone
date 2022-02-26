a2c2d7826c7a9ac153f315e14cc9071d91531ca1
Sabin Ionut <sabin_balan@yahoo.com>
2022-02-26 19:51:28 +0200

    update function_zip_path

--- IaC/lambda_module/main.tf
+++ IaC/lambda_module/main.tf
@@ -1,7 +1,7 @@
 locals {
   function_name = "${var.service_name}Lambda"
   src_folder_name       = basename(var.src_path)
-  function_zip_path     = "${var.src_path}/bin/Release/net6.0/${local.src_folder_name}.zip"
+  function_zip_path     = "${local.src_folder_name}.zip"
 }
 
 resource "aws_iam_role" "LambdaRole" {
